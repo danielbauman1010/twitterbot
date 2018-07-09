9545154c78334ba75f37b1a3224bcc5cc61b01bb
danielbauman1010 <danielbauman1010@gmail.com>
2018-07-08 23:49:32 -0400

    Retweeting works

--- bot.js
+++ bot.js
@@ -5,6 +5,29 @@ var Twitter = new twit(config);
 
 
 
+
+
+/*
+
+Retweet test: Works.
+
+Twitter.get('search/tweets', {q: "#bitcoin", count: 10}, function(err, reply){
+  if(err){
+    console.log(err);
+  } else {
+    var tweet = reply.statuses[0];
+  	Twitter.post('statuses/retweet/:id', { id: tweet.id_str }, function(){
+      console.log("Success.");
+    });
+  }
+});
+
+*/
+
+
+
+
+
 /*
 
 Favorite test: Works.
@@ -21,6 +44,10 @@ Twitter.get('search/tweets', {q: 'hello world! //twitted by a bot', count: 1}, f
 });
 
 */
+
+
+
+
 
 /*
 
