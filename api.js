17cf186ae5217ab04908d4470e539d2766c9cff8
danielbauman1010 <danielbauman1010@gmail.com>
2018-07-20 19:30:46 -0400

    basic search for related hashtags based on mentioning works, run script with terminal and use with html UI.

--- api.js
+++ api.js
@@ -6,7 +6,7 @@ var Twitter = new twit(config);
 function searchTweets(querry, limit) {
   return new Promise(function(resolve,reject){
     var qry = querry +  "-filter:retweets -filter:replies";
-    Twitter.get('search/tweets', {q: qry, tweet_mode: 'extended',count: limit}, function(err, res){
+    Twitter.get('search/tweets', {q: qry, tweet_mode: 'extended', result_type: 'popular',count: limit}, function(err, res){
       if(err){
         reject(err);
       } else {
@@ -17,4 +17,79 @@ function searchTweets(querry, limit) {
   });
 }
 
+function findHashtags(text){
+  hashtags = [];
+  words = text.split(' ');
+  for(i in words) {
+    if(words[i].startsWith("#")) {
+      hashtags.push(words[i]);
+    }
+  }
+  return hashtags;
+}
+
+function expandHashtags(hashtagList, count){
+  return new Promise(function(resolve,reject){
+    expansionStep(hashtagList, {}).then(function(results){
+      resultList = Object.entries(results);
+      resultList = resultList.sort( (hash1, hash2) => {
+        return hash2[1] - hash1[1];
+      });
+      var limit = count;
+      if(limit > resultList.length) {
+        limit = resultList;
+      }
+      let returnedList = [];
+      for (var i = 0; i < limit; i++) {
+       returnedList.push(resultList[i][0]);
+      }
+      resolve(returnedList);
+    }, function(err){
+      reject(err);
+    });
+  });
+}
+
+// Recursive solution:
+function expansionStep(hashtagList, expandedTags){
+
+  return new Promise(function(resolve,reject){
+
+      searchTweets(hashtagList.pop(), 100).then(function(tweets){
+
+        tweets.forEach(tweet => {
+          var tweetText = tweet.full_text;
+          tweetText = tweetText.split('\t').join(' ');
+          tweetText = tweetText.split('\n').join(' ');
+          tweetText = tweetText.split(',').join(' ');
+          tweetText = tweetText.split('.').join(' ');
+          tweetText = tweetText.split('?').join(' ');
+          tweetText = tweetText.split('!').join(' ');
+          tweetText = tweetText.toLowerCase();
+          findHashtags(tweetText).forEach(tag => {
+            if(tag in expandedTags) {
+              expandedTags[tag] += 1;
+            } else {
+              expandedTags[tag] = 1;
+            }
+          });
+        });
+
+        if(hashtagList.length == 0){
+          resolve(expandedTags);
+        } else {
+          resolve(expansionStep(hashtagList, expandedTags));
+        }
+
+      }, function(err){
+        reject(err);
+      });
+
+  });
+
+}
+
 module.exports.searchTweets = searchTweets;
+module.exports.findHashtags = findHashtags;
+module.exports.expandHashtags = expandHashtags;
+module.exports.expansionStep = expansionStep;
--- bot.js
+++ bot.js
@@ -1,13 +1,30 @@
 var api = require('./api.js')
+var express = require('express');
+var path = require('path');
 
-var hashtaglist = ["#dc", "#marvel"];
+var server = express();
 
-api.searchTweets(hashtaglist[0], 10).then(function(results){
-  for(i in results) {
-    console.log("\n"+i);
-    console.log(results[i].full_text);
-    console.log("\n\n\n");
-  }
-},function(err){
-  console.log(err);
+server.get('/', function(req,res){
+  res.sendFile(path.join(__dirname,'index.html'));
 });
+
+server.get('/expand',function(req,res){
+  var hashtagList = req.query.list;
+  var count = Number(req.query.count);
+  api.expandHashtags(hashtagList.split(' '),count).then(function(results){
+    res.end(""+results);
+  },function(err){
+    res.end(err);
+  })
+});
+
+//
+// api.expandHashtags(["#Trump", "#Putin", "#helsinki"], 5).then(function(results){
+//   console.log(results);
+// }, function(err){
+//   console.log(err);
+// })
+
+server.listen(3000);
+console.log("To use, enter the following address. To stop the process, press Ctrl and C simultaneously.");
+console.log("http://localhost:3000/");
